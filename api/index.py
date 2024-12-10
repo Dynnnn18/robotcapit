03c8fbd529b0aae4c29c3a275033f546e017479c
Dynnnn18 <nahdiyin18fx@gmail.com>
2024-12-10 16:15:20 +0700

    update

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, db
 from flask_cors import CORS
 
 # Inisialisasi Firebase Admin SDK
-cred = credentials.Certificate("./api/robotcapit-5ae75-firebase-adminsdk-f1fij-6b16a61c88.json")
+cred = credentials.Certificate("api/robotcapit-5ae75-firebase-adminsdk-f1fij-6b16a61c88.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://robotcapit-5ae75-default-rtdb.asia-southeast1.firebasedatabase.app/'  # Ganti dengan URL Realtime Database Anda
 })
