1e5ebd0190f45925c16fbd420e9b86f17f387f3f
Dynnnn18 <nahdiyin18fx@gmail.com>
2024-12-12 16:22:35 +0700

    update

--- api/index.py
+++ api/index.py
@@ -2,16 +2,47 @@ from flask import Flask, jsonify, request, render_template
 import firebase_admin
 from firebase_admin import credentials, db
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
-# Inisialisasi Firebase Admin SDK
-cred = credentials.Certificate("./api/robotcapit-5ae75-firebase-adminsdk-f1fij-140c171c45.json")
+# Memuat variabel lingkungan dari file .env
+load_dotenv()
+
+# Mendapatkan kredensial dari .env
+FIREBASE_DATABASE_URL = os.getenv("FIREBASE_DATABASE_URL")
+FIREBASE_PROJECT_ID = os.getenv("FIREBASE_PROJECT_ID")
+FIREBASE_PRIVATE_KEY_ID = os.getenv("FIREBASE_PRIVATE_KEY_ID")
+FIREBASE_PRIVATE_KEY = os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n")
+FIREBASE_CLIENT_EMAIL = os.getenv("FIREBASE_CLIENT_EMAIL")
+FIREBASE_CLIENT_ID = os.getenv("FIREBASE_CLIENT_ID")
+FIREBASE_AUTH_URI = os.getenv("FIREBASE_AUTH_URI")
+FIREBASE_TOKEN_URI = os.getenv("FIREBASE_TOKEN_URI")
+FIREBASE_AUTH_PROVIDER_X509_CERT_URL = os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL")
+FIREBASE_CLIENT_X509_CERT_URL = os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+
+# Inisialisasi Firebase Admin SDK menggunakan nilai dari .env
+firebase_config = {
+    "type": "service_account",
+    "project_id": FIREBASE_PROJECT_ID,
+    "private_key_id": FIREBASE_PRIVATE_KEY_ID,
+    "private_key": FIREBASE_PRIVATE_KEY,
+    "client_email": FIREBASE_CLIENT_EMAIL,
+    "client_id": FIREBASE_CLIENT_ID,
+    "auth_uri": FIREBASE_AUTH_URI,
+    "token_uri": FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": FIREBASE_CLIENT_X509_CERT_URL
+}
+
+cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://robotcapit-5ae75-default-rtdb.asia-southeast1.firebasedatabase.app/'  # Ganti dengan URL Realtime Database Anda
+    'databaseURL': FIREBASE_DATABASE_URL
 })
 
 # Inisialisasi Flask
 app = Flask(__name__)
 CORS(app)
+
 # Route untuk mendapatkan semua data dari path tertentu
 @app.route('/get-data/<path>', methods=['GET'])
 def get_data(path):
@@ -64,11 +95,10 @@ def update_data(path):
         return jsonify({"message": "Data updated successfully"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 @app.route('/')
-
-def main ():
+def main():
     return render_template('Arena.html')
-
 
 # Menjalankan server Flask
 if __name__ == '__main__':
